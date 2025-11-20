3d64ee3a952848e478fdd1b334be4c00e607550d
cmonsalt <cristiantm12@gmail.com>
2025-11-19 19:30:16 -0500

    restructuracion carpeta api

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,12 @@
 console.log("CamAssist loaded!");
+
+// Test button (temporal)
+setTimeout(() => {
+  fetch('https://camassist.vercel.app/api/generate', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({message: 'test'})
+  })
+  .then(r => r.json())
+  .then(data => console.log('API:', data));
+}, 2000);
